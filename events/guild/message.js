67fc651b5bfdb78058fd99119b1f03c298ef9666
MadlyJohn <68296409+MadlyJohn@users.noreply.github.com>
2020-08-28 14:00:21 +0100

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,26 +1,34 @@
-module.exports = async (bot, message) => {
-    const prefix = ".";
+const Discord = require("discord.js"), cooldowns = new Discord.Collection();
 
-    // If the message was sent by a bot, doesn't start with the prefix, or wasn't in a guild, return
-    if (
-        message.author.bot ||
-        !message.content.startsWith(prefix) ||
-        !message.guild
-    )
-        return;
-    // If there is no message member, fetch the member
-    if (!message.member)
-        message.member = await message.guild.fetchMember(message);
+module.exports = async (client, message) => {
+    if (message.author.bot || message.author === client.user) return;
 
-    // Variables
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-    if (cmd.length === 0) return;
-    let command = bot.commands.get(cmd);
+    let prefix = "."
 
-    // Makes aliases work, don't remove
-    if (!command || !command.run)
-        command = bot.commands.get(bot.aliases.get(cmd));
 
-    command.run(bot, message, args);
-};
+
+    if (!message.content.startsWith(prefix)) return;
+
+    let args = message.content.slice(prefix.length).trim().split(/ +/g);
+    let msg = message.content.toLowerCase();
+    let cmd = args.shift().toLowerCase();
+    let sender = message.author;
+
+    message.flags = []
+    while (args[0] && args[0][0] === "-") {
+        message.flags.push(args.shift().slice(1));
+    }
+    
+    let commandFile = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
+    if (!commandFile) return;
+    
+   
+    try {
+        if (!commandFile) return;
+        commandFile.run(client, message, args);
+    } catch (error) {
+        console.log(error.message);
+    } finally {
+        console.log(`${sender.tag} (${sender.id}) ran a command: ${cmd}`);
+    }
+}
